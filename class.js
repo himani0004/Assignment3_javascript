fc012f66689645099f4e03081d2e932dce2a9c4c
Himani Sharma <99091480+himani0004@users.noreply.github.com>
2023-07-19 12:15:13 +0530

    Add files via upload

---
+++ class.js
@@ -0,0 +1,77 @@
+// Number function------------->>>>>>>>
+// const var1 = 34.5
+// const var2=Number(var1)
+// console.log(var2);
+
+
+// Number to string ---------------------------->>>>>>>>>>>>>
+// const var1 = 34.5
+// const var2=String(var1)
+// console.log(var2);
+// console.log (typeof var2);
+
+ 
+// string to number--------------------->>>>>>>>>>>>>>>>>>>>>
+// const var3 ="himani" 
+// const var4=Number(var3)
+// console.log(var4);
+
+
+
+//empty value give using number---------------------------->>>>>>>>>>>>>>
+// const var5 = " "
+// const var6 = Number(var5)
+// console.log(var6)
+
+
+
+// adding multiple values using string------------------------------>>>>>>>>>>>>>>>>
+//  const var6 = 125
+//  const var7= String(var6 + 50 + 55)   ;
+//  console.log(var7)
+ 
+
+//number to string using to.string()----------------------------------->>>>>>>>>>>>>>>>>>>>
+// const var8 = 22
+// console.log (var8.toString())
+// console.log(typeof var8);
+
+
+
+// string to number using parseint()----------------------------->>>>>>>>>>>>>>>>>>>
+// const s = "234"
+// console.log(typeof s);
+// const n = parseInt(s);
+// console.log(n);
+// console.log(typeof(n));
+
+//string to float value-------------------->>>>>>>>>>>
+// const x = "23.4"
+// console.log(typeof x);
+// const y = parseFloat(x);
+// console.log(y);
+// console.log(typeof(y));
+
+
+
+//unary operator changing  string to number -------->>>>>>>>>>>>>>
+// const a = "33"
+// const h = +a;
+// console.log(h);
+// console.log(typeof(h));
+
+
+
+//string to boolean value
+// let t = "";
+// let u = !!(t); 
+// console.log(u);
+// console.log(typeof(u));
+
+// let c = "Himani";
+// let d = !!(c); 
+// console.log(d);
+// console.log(typeof(d));
+
+
+
